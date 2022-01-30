a94a9ae3314084da17d198b6d7c12a4589ba6707
gdssvpp <pablonogueira16@hotmail.com>
2022-01-29 22:22:41 -0300

    alooo

--- src/BancoDigital/ContaCorrente.java
+++ src/BancoDigital/ContaCorrente.java
@@ -3,6 +3,6 @@ package BancoDigital;
 public class ContaCorrente {
     public static void main(String[] args) {
 
-        System.out.println("Teste de função");
+        System.out.println("Teste das funções");
     }
 }
